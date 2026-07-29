d14c90d01bc8868cad7f22a9ee551007a61f0d54
Mariano_Javier_de_Leon_Dominguez_Romero <mariano.javier.deleon@gmail.com>
2026-07-29 00:50:04 -0300

    fix(whitesmoke): update SUBMISSION_URL to mardom/pz_data_challenge v2.0.0 release

--- tests/test_whitesmoke.py
+++ tests/test_whitesmoke.py
@@ -30,7 +30,7 @@ SUBMISSION_NAME: str = "whitesmoke"
 # SUBMISSION_URL points to the release v2.0.0 tarball for whitesmoke mixture of experts
 SUBMISSION_URL: str = os.environ.get(
     "WHITESMOKE_SUBMISSION_URL",
-    "https://github.com/ymuza/pz_challenge/releases/download/v2.0.0/whitesmoke.tgz"
+    "https://github.com/mardom/pz_data_challenge/releases/download/v2.0.0/whitesmoke.tgz"
 )
 
 # don't change these
